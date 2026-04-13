45ec1b25960c17ba8307792a12b06f6e45bd4cd4
chanikya5793 <s584483@nwmissouri.edu>
2026-04-13 15:08:56 -0500

    Refactor code structure for improved readability and maintainability

--- app.config.js
+++ app.config.js
@@ -1,22 +1,8 @@
 module.exports = {
-  name: 'metaHub',
-  displayName: 'metaHub',
   expo: {
     name: 'metaHub',
     slug: 'metahub',
     version: '1.0.0',
-    orientation: 'portrait',
-    userInterfaceStyle: 'light',
-    scheme: 'metahub',
-    platforms: ['ios', 'android'],
-    ios: {
-      supportsTablet: true,
-      bundleIdentifier: 'com.metahub',
-    },
-    android: {
-      package: 'com.metahub',
-    },
-    plugins: ['@react-native-firebase/app', 'expo-asset'],
     extra: {
       eas: {
         projectId: 'eb7b4461-08b7-4a6f-b255-d99efdfadc55',
