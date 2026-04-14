ebeb9173404b100eb8c68b1eba1ddbf9be9e3167
chanikya5793 <s584483@nwmissouri.edu>
2026-04-14 11:15:15 -0500

    feat: Add Android package configuration to app.config.js

--- app.config.js
+++ app.config.js
@@ -3,6 +3,9 @@ module.exports = {
     name: 'metaHub',
     slug: 'metahub',
     version: '1.0.0',
+    android: {
+      package: 'com.metahub',
+    },
     extra: {
       eas: {
         projectId: 'eb7b4461-08b7-4a6f-b255-d99efdfadc55',
